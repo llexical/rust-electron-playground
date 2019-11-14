21f91ae987aeca7c690316ec76f8be53567efd0a
Lizzie <lilibuth.davisone@gmail.com>
2019-11-14 19:24:18 +0000

    Add Cargo.toml to allow editors to work, also broke everything
    Previously all editors borked because there was not a Cargo.toml in the
    root so I was working in create confusion. This commit brings great
    light to our lives.
    
    Also broke everything. Not sure why.

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,12 +1,134 @@
 #[macro_use]
 extern crate neon;
+extern crate reqwest;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 
 use neon::prelude::*;
+use reqwest::Client;
 
-fn thread_count(mut cx: FunctionContext) -> JsResult<JsNumber> {
-    Ok(cx.number(num_cpus::get() as f64))
+#[derive(Deserialize, Debug)]
+struct RegisterAuthResponse {
+    session: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct RegisterResponse {
+    user_id: String,
+    home_server: String,
+    access_token: String,
+    device_id: String,
+}
+
+fn register_user(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let username = cx.argument::<JsString>(0)?.value();
+    let password = cx.argument::<JsString>(1)?.value();
+
+    let session_token = match make_auth_request() {
+        Err(e) => {
+            let success = cx.boolean(false);
+            let error_msg = cx.string(handler(e));
+
+            let response_obj = cx.empty_object();
+            response_obj.set(&mut cx, "success", success)?;
+            response_obj.set(&mut cx, "error_msg", error_msg)?;
+
+            return cx.throw(response_obj);
+        }
+        Ok(response) => response.session,
+    };
+
+    let _register_response = match make_register_request(username, password, session_token) {
+        Err(e) => {
+            let success = cx.boolean(false);
+            let error_msg = cx.string(handler(e));
+
+            let response_obj = cx.empty_object();
+            response_obj.set(&mut cx, "success", success)?;
+            response_obj.set(&mut cx, "error_msg", error_msg)?;
+
+            return cx.throw(response_obj);
+        }
+        Ok(response) => response,
+    };
+
+    let success = cx.boolean(true);
+    let response_obj = cx.empty_object();
+    response_obj.set(&mut cx, "success", success)?;
+
+    return Ok(response_obj);
+}
+// Response is not a json object conforming to the Simple struct
+fn make_auth_request() -> Result<RegisterAuthResponse, reqwest::Error> {
+    let request_url = "http://my.matrix.host:8008/_matrix/client/r0/register?kind=user";
+    let request_body = json!({});
+
+    return Client::new()
+        .post(request_url)
+        .json(&request_body)
+        .send()?
+        .json();
+}
+
+fn make_register_request(
+    username: String,
+    password: String,
+    session_token: String,
+) -> Result<RegisterResponse, reqwest::Error> {
+    let request_url = "http://my.matrix.host:8008/_matrix/client/r0/register?kind=user";
+    let request_body = json!({
+      "auth": {
+        "session": session_token,
+        "type": "m.login.dummy"
+      },
+      "username": username,
+      "password": password,
+      "initial_device_display_name": "rustapp"
+    });
+
+    let mut response = Client::new().post(request_url).json(&request_body).send();
+
+    let mut response = match response {
+        Err(e) => println!("Error here"),
+        Ok(response) => response,
+    };
+
+    println!("response {}", response.status());
+    return response.json();
+}
+
+fn handler(e: reqwest::Error) -> String {
+    if e.is_http() {
+        match e.url() {
+            None => return format!("No Url given"),
+            Some(url) => return format!("Problem making request to: {}", url),
+        }
+    }
+    if e.is_client_error() {
+        return format!(
+            "There was a {} error making the request to the server",
+            e.status().unwrap()
+        );
+    }
+    if e.is_server_error() {
+        return format!("There was an {} error from the server", e.status().unwrap());
+    }
+    // Inspect the internal error and output it
+    if e.is_serialization() {
+        match e.get_ref() {
+            None => return format!("problem parsing information"),
+            Some(err) => return format!("problem parsing information {}", err),
+        };
+    }
+    if e.is_redirect() {
+        return format!("server redirecting too many times or making loop");
+    }
+
+    return format!("There was an error making your request");
 }
 
 register_module!(mut cx, {
-    cx.export_function("thread_count", thread_count)
+    cx.export_function("register_user", register_user)
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const native = require('../native')
 
-console.log(native.thread_count())
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
